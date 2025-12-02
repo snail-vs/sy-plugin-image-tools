4c7e0ec7a914fa5d62050e6faca3ce4cedacfea3
oniu <oniuss@163.com>
2025-12-02 14:13:14 +0800

    feat: Remove README.md from build output. - Add icon.png and preview.png

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ echo "Copying files..."
 cp -f dist/index.js $plugin_dir
 cp -f dist/styles.css $plugin_dir/index.css
 cp -f plugin.json $plugin_dir
-cp -f README.md $plugin_dir
-cp -f README_zh_CN.md $plugin_dir
+cp -f icon.png $plugin_dir
+cp -f preview.png $plugin_dir
 
 echo "Zipping plugin..."
 zip -r $plugin_dir.zip $plugin_dir
